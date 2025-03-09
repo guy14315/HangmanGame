445e32341925249a7cc381696b90ea4ffcaec7b3
kobkoy <guygame97@gmail.com>
2025-03-09 21:42:43 +0700

    update CommonConstants.

--- src/CommonConstants.java
+++ src/CommonConstants.java
@@ -1,15 +1,13 @@
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
 
 public class CommonConstants {
-    
-    //public static final String DATA_PATH = "resource/data.txt";
-    public static final String IMAGE_PATH = "resource/1.png";
-    
+
+    public static final Dimension FRAME_SIZE = new Dimension(540,760);
+    public static final Dimension MENU_BOTTON_SIZE = new Dimension(150,200);
+    public static final String IMAGE_PATH = "resources/1.png";
     public static final Color PRIMARY_COLOR = Color.decode("#14212D");
     public static final Color SECONDARY_COLOR = Color.decode("#FCA311");
     public static final Color BACKGROUND_COLOR = Color.decode("#101820");
-
-
-    public static final Dimension FRAME_SIZE = new Dimension(549, 760);
 
 }
